777cae30ca4120069036b2f4e14ebb648ac04290
kubakubelek <kuba.zwiewka@gmail.com>
2024-04-17 23:22:20 +0200

    ,

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,5 +1,16 @@
-import sys
-import os
-sys.path.append('/home/kubakubelek/django_projects')
-sys.path.append('/home/kubakubelek/django_projects/mysite')
+"""
+WSGI config for mysite project.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
