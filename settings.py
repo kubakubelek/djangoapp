88707d890a2e6475e8e08d55f4e13714d7025911
kubakubelek <156833886+kubakubelek@users.noreply.github.com>
2024-05-04 23:04:44 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -33,7 +33,7 @@ LOGIN_REDIRECT_URL = '/'
 
 # Support env variables from .env file if defined
 import os
-from dotenv import load_dotenv
+
 env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
 load_dotenv(env_path)
 # SECURITY WARNING: keep the secret key used in production secret!
