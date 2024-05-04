a6fc73e2ed732a6e64510dbf0468df6d131180b6
kubaz <kuba.zwiewka@gmail.com>
2024-05-04 17:01:34 +0200

    commit

--- mysite/kaczacze/templatetags/custom_filters.py
+++
@@ -1,6 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def get_vote_value(vote_values, post_id):
-    return vote_values.get(post_id, "Brak głosu")
--- mysite/wsgi.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-sys.path.append('/home/kubakubelek/django_projects')
-sys.path.append('/home/kubakubelek/django_projects/mysite')
-
